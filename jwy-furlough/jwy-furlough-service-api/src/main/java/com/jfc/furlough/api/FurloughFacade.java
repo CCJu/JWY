bf8209744494871e6aee0564820605aee6b1bba5
CCJu <935166424@qq.com>
2020-01-28 15:19:17 +0800

    新增请假接口

---
+++ jwy-furlough/jwy-furlough-service-api/src/main/java/com/jfc/furlough/api/FurloughFacade.java
@@ -0,0 +1,16 @@
+/*
+ * frxs Inc.  湖南兴盛优选电子商务有限公司.
+ * Copyright (c) 2017-2019. All Rights Reserved.
+ */
+package com.jfc.furlough.api;
+
+/**
+ * 请假模块门面类
+ *
+ * @author juchengcheng
+ * @version : FurloughFacade.java,v 0.1 2020年01月28日 15:16
+ */
+public interface FurloughFacade {
+
+
+}
---
+++ jwy-furlough/jwy-furlough-service-api/src/main/java/com/jfc/furlough/domain/FurloughRequest.java
@@ -0,0 +1,13 @@
+/*
+ * frxs Inc.  湖南兴盛优选电子商务有限公司.
+ * Copyright (c) 2017-2019. All Rights Reserved.
+ */
+package com.jfc.furlough.domain;
+
+/**
+ * @author juchengcheng
+ * @version : FurloughRequest.java,v 0.1 2020年01月28日 15:18
+ */
+public class FurloughRequest {
+
+}
