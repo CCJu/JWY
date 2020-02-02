89fd4af12d6bc9d62d0ea78ff452d88c4da12667
CCJu <935166424@qq.com>
2020-02-02 13:14:01 +0800

    1. 修改包结构

--- jwy-furlough/jwy-furlough-service-api/src/main/java/com/jwy/furlough/domain/FurloughResponse.java
+++ jwy-furlough/jwy-furlough-service-api/src/main/java/com/jwy/furlough/domain/FurloughResponse.java
@@ -2,12 +2,12 @@
  * frxs Inc.  湖南兴盛优选电子商务有限公司.
  * Copyright (c) 2017-2019. All Rights Reserved.
  */
-package com.jfc.jwy.furlough.domain;
+package com.jwy.furlough.domain;
 
 /**
  * @author juchengcheng
- * @version : FurloughRequest.java,v 0.1 2020年01月28日 15:18
+ * @version : FurloughResponse.java,v 0.1 2020年01月28日 15:18
  */
-public class FurloughRequest {
+public class FurloughResponse {
 
 }
